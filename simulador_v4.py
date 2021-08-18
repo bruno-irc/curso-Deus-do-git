acc98707e7e76a8e9470d324f16d25582d959865
bruno-irc <bruno.lopes@irricontrol.com.br>
2021-08-18 11:44:46 -0300

    ajustes no simulador v4

--- simulador_v4.py
+++ simulador_v4.py
@@ -7,6 +7,7 @@ def envia_lat_long_v4(latitude, longitude):
         'type': 'tx',
         'frame_id': frame_id,
         'dest_addr': Controlador,
-        'data': DATA
+        'data': DATA,
+        'none': None
     }
     radio.send_xbee_frame(g)
